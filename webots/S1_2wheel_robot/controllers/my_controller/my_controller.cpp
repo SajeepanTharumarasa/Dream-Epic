63660295135411e3195e4b972f0d757e906eb4a7
Sajeepan <Sajeepan1299@gmail.com>
2021-05-06 09:32:28 +0530

    updated

--- webots/S1_2wheel_robot/controllers/my_controller/my_controller.cpp
+++ webots/S1_2wheel_robot/controllers/my_controller/my_controller.cpp
@@ -1,12 +1,10 @@
 #include <webots/Robot.hpp>
-
-// Added a new include file
 #include <webots/Motor.hpp>
 
 #define TIME_STEP 32
 
 
-#define MAX_SPEED 10
+#define MAX_SPEED 7.5
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
@@ -19,10 +17,20 @@ int main(int argc, char **argv) {
  Motor *rightMotor = robot->getMotor("right_motor");
  leftMotor->setPosition(INFINITY);
  rightMotor->setPosition(INFINITY);
+ leftMotor->setVelocity(0 * MAX_SPEED);
+ rightMotor->setVelocity(0 * MAX_SPEED);
+ 
+ //rotating distance sensor motor
+ Motor *bottom_sensor = robot->getMotor("piller_detect_bottom");
+ //Motor *top_sensor = robot->getMotor("right_motor");
+ bottom_sensor->setPosition(INFINITY);
+ //top_sensor->setPosition(INFINITY);
 
  // set up the motor speeds at 10% of the MAX_SPEED.
- leftMotor->setVelocity(0.9 * MAX_SPEED);
- rightMotor->setVelocity(0.9 * MAX_SPEED);
+ bottom_sensor->setVelocity(-0.5 * MAX_SPEED);
+ 
+ //leftMotor->setVelocity(-0.9 * MAX_SPEED);
+ //rightMotor->setVelocity(0.9 * MAX_SPEED);
 
  while (robot->step(TIME_STEP) != -1);
  delete robot;
