af4118bf0d1f9627b78de47041a5db9c1caef3fd
Sajeepan <Sajeepan1299@gmail.com>
2021-05-11 14:55:45 +0530

    Updated

--- webots/S1_2wheel_robot/controllers/Dream_epic_contoller/Dream_epic_contoller.cpp
+++ webots/S1_2wheel_robot/controllers/Dream_epic_contoller/Dream_epic_contoller.cpp
@@ -3,15 +3,20 @@
 #include <webots/Camera.hpp>
 #include <webots/DistanceSensor.hpp>
 #include <iostream>
+using namespace webots;
 
 #define TIME_STEP 32
-#define MAX_SPEED 7.5
-#define petch_center_time  1
-#define turn_move_time 1
-#define turn_time 1
-
-using namespace webots;
+#define MAX_SPEED 7.5    // anguler velocity
+#define robot_length 12  // in cm
+#define robot_width 10   // in cm
+#define wheel_radius 2   // in cm
+#define petch_lenght 15  // in cm 
+   
+double petch_center_time = 0.8*petch_lenght/(MAX_SPEED*wheel_radius);
+double turn_90time = 3.14*(robot_width/2)/(MAX_SPEED*wheel_radius);
+ 
 int step=0;
+int junction_turn[2] ={-1,0}; 
 //inticial device setting
 Robot *robot = new Robot();
 Motor *leftMotor = robot->getMotor("left_motor");
@@ -21,18 +26,14 @@ DistanceSensor *sensor_right = robot->getDistanceSensor("right_ir");
 DistanceSensor *sensor_left = robot->getDistanceSensor("left_ir");
 DistanceSensor *sensor_center = robot->getDistanceSensor("center_ir");
      
-int junction_turn[2] ={-1,0}; 
-
-
 void colour_detecting(){
       Camera *camera_s = robot->getCamera("camera");
       camera_s->enable(TIME_STEP);
       const int width = camera_s->getWidth();
       const int height = camera_s->getHeight();
-     
+     std::cout << "petch colour is:  " << std::endl;
       robot->step(TIME_STEP);
           const unsigned char *image =camera_s->getImage();
-          
           int red=0, blue=0, green=0;
           for (int i = width / 3; i < 2 * width / 3; i++) {
                 for (int j = height / 2; j < 3 * height / 4; j++) {
@@ -42,19 +43,14 @@ void colour_detecting(){
                 }}
            camera_s->disable();
            if (red>(blue+green)){
-               std::cout << "petch colour is: Red " << std::endl;
-               return;}
+               std::cout << "petch colour is: Red " << std::endl;}
            else if(blue>(red+green)){
-               std::cout << "petch colour is: blue " << std::endl;
-               return;}
+               std::cout << "petch colour is: blue " << std::endl;}
            else if(green>(blue+red)){
-               std::cout << "petch colour is: green " << std::endl;
-               return; }
-           else{ 
-               return;}
-   }
-   
-   
+               std::cout << "petch colour is: green " << std::endl; }
+           return;
+   }   
+//find the right and left piller values   
 int find_value(double top, double bottom){
          int size;double value;
          if (top<990){
@@ -64,15 +60,14 @@ int find_value(double top, double bottom){
                size=1;
                value = bottom;} 
          if (value>410 && value<510 ) {
-             size = size*4;} 
+               size = size*4;} 
          else if (value>575 && value<675 ) {
-             size = size*8;} 
+               size = size*8;} 
          else if (value>740 && value<840 ) {
-             size = size*12;} 
+               size = size*12;} 
      return size;
-     
      }
-
+//detect the distance and piller height
 int piller_detecting() {
     DistanceSensor *sensor_top = robot->getDistanceSensor("top_sensor");
     DistanceSensor *sensor_bottom = robot->getDistanceSensor("bottom_sensor");
@@ -98,18 +93,16 @@ int piller_detecting() {
                 sensorMotor->setVelocity(MAX_SPEED);}
             else{
                 sensorMotor->setVelocity(0);
-            break;}         
+                break;}         
      }
      robot->step(TIME_STEP) ;
      value_top = sensor_top->getValue();
      value_bottom = sensor_bottom->getValue();
      int value_right = find_value(value_top,value_bottom);
-     int x =(abs(value_right-value_left));
-     std::cout << "petch  " << x << std::endl;
-     x=(x/4)-2;
+     int x =(abs(value_right-value_left))/4-2;
      return x;
 }
-
+// turn the robot 90' right or left to given direction
 void turn_90(int direction){
      double left_speed =0;
      double right_speed =0;
@@ -120,9 +113,10 @@ void turn_90(int direction){
           left_speed=MAX_SPEED;
           right_speed=-1*MAX_SPEED;}
      else {return;}
-     double start_time= robot->getTime();
-     double time = start_time+(2*3.14/6);
+     
+     double time = robot->getTime()+turn_90time;
      double current_time=0;
+     
      while (robot->step(TIME_STEP) != -1) {
           current_time = robot->getTime();
           if (time>current_time){
@@ -132,16 +126,13 @@ void turn_90(int direction){
      }
      return;    
      }
-
-void move_center(double time){
-     double start_time= robot->getTime();
-     time+=start_time;
-     //std::cout << time << std::endl;
+// move the robot in forword direction during the time
+void move_center(double time){ 
+     time += robot->getTime();;
      double current_time=0;
+     
      while (robot->step(TIME_STEP) != -1) {
-          
           current_time= robot->getTime();
-          //std::cout << current_time << std::endl;
           if (time>current_time){
               leftMotor->setVelocity( MAX_SPEED);
               rightMotor->setVelocity( MAX_SPEED);}
@@ -151,29 +142,26 @@ void move_center(double time){
 }
 
 void stop(){
-    leftMotor->setVelocity(0* MAX_SPEED);
-    rightMotor->setVelocity(0* MAX_SPEED);
+    leftMotor->setVelocity(0);
+    rightMotor->setVelocity(0);
 }
 
 void forword_petch(int step){
           if (step==2){
-              move_center(0.9);
+              move_center(petch_center_time);
               colour_detecting();}
           else if (step==3) {
-              move_center(0.9);
+              move_center(petch_center_time);
               stop();
              junction_turn[1] = piller_detecting();}
           else if(step==4 || step==5){
-              move_center(0.33);
-              //std::cout << junction_turn[step-4] << std::endl;
-              turn_90(junction_turn[step-4]);
-              }
+              move_center(0.27);
+              turn_90(junction_turn[step-4]);}
           else if(step==6){
-              move_center(1);
+              move_center(petch_center_time);
               stop();
               return;}               
       while (robot->step(TIME_STEP) != -1) {
-          
           const double value_right = sensor_right->getValue();
           const double value_left = sensor_left->getValue();
           const double value_center = sensor_center->getValue();
@@ -184,16 +172,22 @@ void forword_petch(int step){
               break;}
       } 
     return;
-}   
+} 
+void factor_N(int value){
+    for(int i=1;i<=value;i++){
+        if (value%i==0){
+        std::cout << i << std::endl;}
+    }
+
+}  
    
 int main(int argc, char **argv) {
-      DistanceSensor *sensor_left2 = robot->getDistanceSensor("left2_ir");
-      DistanceSensor *sensor_right2 = robot->getDistanceSensor("right2_ir");
+     
+     DistanceSensor *sensor_left2 = robot->getDistanceSensor("left2_ir");
+     DistanceSensor *sensor_right2 = robot->getDistanceSensor("right2_ir");
      leftMotor->setPosition(INFINITY);
      rightMotor->setPosition(INFINITY);
-     leftMotor->setVelocity(0 * MAX_SPEED);
-     rightMotor->setVelocity(0 * MAX_SPEED);
-     
+     stop();
      // get a handler to the motors and set target position to infinity (speed control)
      sensor_right->enable(TIME_STEP);
      sensor_left->enable(TIME_STEP);
@@ -209,7 +203,7 @@ int main(int argc, char **argv) {
     const double l_2 = sensor_left2->getValue();
     
     
-    if (c_0<420 && r_1<420 && l_1<420){
+    if (abs(c_0-r_1)<20 && abs(c_0-l_1)<20){
           step+=1;
           forword_petch (step);
           if(step==6){break;}}
@@ -223,16 +217,24 @@ int main(int argc, char **argv) {
          leftMotor->setVelocity( -0.5*MAX_SPEED);
          rightMotor->setVelocity(MAX_SPEED);}
     else if(r_1>900 && r_2>900 && c_0<420 && l_1<420 && l_2<420){
-         move_center(0.33);
+         move_center(0.27);
          turn_90(-1);}
     else if(r_1<420 && r_2<420 && c_0<420 && l_1>900 && l_2>900){
-         move_center(0.33);
+         move_center(0.27);
          turn_90(1);}      
     else{break;}
   };
-  // Enter here exit cleanup code.
-
+  sensor_right->disable();
+  sensor_left->disable();
+  sensor_center->disable();
+  sensor_right2->disable();
+  sensor_left2->disable();
+  std::cout <<junction_turn[1] << std::endl;
+  //factor_N((junction_turn[1]+2)*4);
   delete robot;
+  double time = robot->getTime();
+  std::cout <<time << std::endl;
+  
   return 0;
 }
 
