55da537119ebecdbd45049535a566fd0460a6b9f
Sajeepan <Sajeepan1299@gmail.com>
2021-11-02 08:34:06 +0530

    unnecesseary files are deleted

--- virtual_round_1/S1_2wheel_robot/controllers/my_controller/build/release/my_controller.d
+++
@@ -1,8 +0,0 @@
-build/release/my_controller.o: my_controller.cpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- D:/Program\ Files/Webots/include/controller/c/webots/types.h \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
- D:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h \
- D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
--- virtual_round_1/S1_2wheel_robot/controllers/my_controller_camera/build/release/my_controller_camera.d
+++
@@ -1,8 +0,0 @@
-build/release/my_controller_camera.o: my_controller_camera.c \
- D:\Program\ Files\Webots/include/controller/c/webots/camera.h \
- D:\Program\ Files\Webots/include/controller/c/webots/camera_recognition_object.h \
- D:\Program\ Files\Webots/include/controller/c/webots/types.h \
- D:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- D:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- D:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- D:\Program\ Files\Webots/include/controller/c/webots/utils/system.h
